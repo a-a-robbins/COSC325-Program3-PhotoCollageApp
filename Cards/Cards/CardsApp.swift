d849a02fc9434e9769f4d9896b9f902cbfedc8ae
Ashley Robbins <robbinsa1@central.edu>
2023-02-23 16:47:35 -0600

    create new Xcode project

---
+++ Cards/Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by ARobbins on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
